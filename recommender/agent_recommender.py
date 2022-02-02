9440b00970c4b55ea21b67f571ee7df2fe4c48af
timruhkopf <timruhkopf@gmail.com>
2022-02-02 10:26:17 +0100

    moved agent_recommender.py in its own directory

---
+++ recommender/agent_recommender.py
@@ -0,0 +1,14 @@
+from sample_code_submission.agent import Agent
+
+
+class Agent_Recommender(Agent):
+    def meta_train(self,
+                   dataset_meta_features,
+                   algorithms_meta_features,
+                   validation_learning_curves,
+                   test_learning_curves):
+        pass
+
+    def suggest(self, observation):
+        pass
+
