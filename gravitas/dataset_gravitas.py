4bd009d72862533182da7177ddb670f20122fe72
timruhkopf <timruhkopf@gmail.com>
2022-02-07 12:54:19 +0100

    (A) refactoring o dataset_gravitas.py: 1) split into multiple functions the preprocessing steps 2) changed the dictionary keys to int where appropriate
    (B) added scalar performance df preprocessing - with quantile regression in mind

--- gravitas/dataset_gravitas.py
+++ gravitas/dataset_gravitas.py
@@ -8,29 +8,20 @@ from itertools import product
 
 
 class Dataset_Gravity(Dataset):
-    def __init__(
-            self, dataset_meta_features, learning_curves, algorithms_meta_features
-    ):
-        self.preprocess(
-            dataset_meta_features, learning_curves, algorithms_meta_features
-        )
+    def __init__(self, dataset_meta_features, learning_curves, algorithms_meta_features, no_competitors=11):
+        """
 
-        # create pairwise datasets
-        # indicies = dataset_meta_features.keys()
-        # self.data_indicies = list(tup for tup in product(range(self.nD), range(self.nD)) if tup[0] != tup[1])
+        :param dataset_meta_features:
+        :param learning_curves:
+        :param algorithms_meta_features:
+        :param no_competitors: number of datasets that are compared against. Notice that no_competitors = 2
+        is pairwise comparisons
+        """
+        self.no_competitors = no_competitors
+        self.preprocess(dataset_meta_features, learning_curves, algorithms_meta_features)
 
     def __len__(self):
         return len(self.datasets_meta_features)
-
-    # def __getitem__(self, item):
-    #
-    #     idx = self.data_indicies[item]
-    #     D0, D1 = self.datasets_meta_features[idx[0]], self.datasets_meta_features[idx[1]]
-    #
-    #     # learning_properties
-    #     A0, A1 = self.algo_performances[idx[0]], self.algo_performances[idx[1]]
-    #
-    #     return D0, D1, A0, A1
 
     def __getitem__(self, item):
         """
@@ -49,7 +40,7 @@ class Dataset_Gravity(Dataset):
         # generate a random compare set
         # TODO move k to init
         # TODO seedit
-        item_compareset = random.choices(list(set(range(self.nD)) - {item}), k=11)
+        item_compareset = random.choices(list(set(range(self.nD)) - {item}), k=self.no_competitors)
         D1 = self.datasets_meta_features[item_compareset]
         A1 = self.algo_performances[item_compareset]
 
@@ -58,9 +49,9 @@ class Dataset_Gravity(Dataset):
     def preprocess(
             self,
             dataset_meta_features,
-            validation_learning_curves,
+            learning_curves,
             algorithms_meta_features,
-            k=0,
+            k=3,
     ):
         """
         (1) Dataset Meta Features are selected based on being variable and transformed to tensor + normalized
@@ -69,48 +60,46 @@ class Dataset_Gravity(Dataset):
         (4) Dataset Features LC properties aggregated by dataset
 
         :param dataset_meta_features:
-        :param validation_learning_curves:
+        :param learning_curves:
         :param algorithms_meta_features:
         :param k: (optional) set the k least performing algorithm performances to 0
         :return:
         """
         self.nD = len(dataset_meta_features.keys())
 
-        # Preprocess dataset meta data (remove the indiscriminative string variables)
-        self.df_data_meta_features = pd.DataFrame(
-            list(dataset_meta_features.values()), index=dataset_meta_features.keys()
-        )
-        string_typed_variables = [
-            "usage", "name", "task", "target_type", "feat_type", "metric",
-        ]
-        other_columns = list(
-            set(self.df_data_meta_features.columns) - set(string_typed_variables)
-        )
-        self.df_data_meta_features[other_columns] = self.df_data_meta_features[
-            other_columns
-        ].astype(float)
+        # changing keys to int
+        algorithms_meta_features = {int(k): v for k, v in algorithms_meta_features.items()}
+        dataset_meta_features = {int(k): v for k, v in dataset_meta_features.items()}
+        learning_curves = {int(k): {int(k1): v1 for k1, v1 in v.items()}
+                           for k, v in learning_curves.items()}
 
-        # min-max normalization of numeric features
-        df = self.df_data_meta_features[other_columns]
-        self.df_data_meta_features[other_columns] = (df - df.min()) / (
-                df.max() - df.min()
-        )
-        self.datasets_meta_features = torch.tensor(
-            self.df_data_meta_features[other_columns].values, dtype=torch.float32
-        )
+        self._preprocess_meta_features(dataset_meta_features)
+        self._preprocess_learning_curves(algorithms_meta_features, learning_curves)
+        self._preporcess_scalar_properties(self.algo_learning_curves)
+        self._preprocess_dataset_properties(learning_curves, dataset_meta_features)
+        self._preprocess_thresholded_algo_performances(k=10)
+        # # amount of time on the datasets to find out which algo works best:
+        # plt.hist(self.df_data_meta_features['time_budget'])
+        # plt.show()
+        #
+        # plt.hist([datum.topk_available_trials for datum in self.dataset_learning_properties.values()])
+        # plt.show()
 
+    def _preprocess_learning_curves(self, algorithms_meta_features, learning_curves):
+        """Enrich the learning curve objects with properties computed on the learning curve
+        such as what the final performance is or when it reached the 90% convergence. etc."""
         # compute algorithm properties -----------------------------------------
         # compute learning curve properties.
 
         # find the 90% quantile in performance
         # ---> learn how fast an algo given a dataset complexity will approx converge?
-        curve_set = validation_learning_curves
-        self.algo_valid_learning_curves = {
+
+        self.algo_learning_curves = {
             k: {} for k in algorithms_meta_features.keys()
         }
-        for ds_id in curve_set.keys():
-            for algo_id, curve in curve_set[ds_id].items():
-                self.algo_valid_learning_curves[algo_id][ds_id] = curve
+        for ds_id in learning_curves.keys():
+            for algo_id, curve in learning_curves[ds_id].items():
+                self.algo_learning_curves[algo_id][ds_id] = curve
 
                 # final performance
                 curve.final_performance = curve.scores[-1]
@@ -126,12 +115,24 @@ class Dataset_Gravity(Dataset):
                 curve.convergence90_time = curve.timestamps[curve.convergence90_step]
                 curve.convergence90_performance = curve.scores[curve.convergence90_step]
 
+    def _preporcess_scalar_properties(self, algo_curves):
+        """
+        calcualte scalar values for each algorithm, dataset combination
+
+        :attributes: must be a single matrix [datasets, algorithms]
+            -algo_convergences90_time: the time at which the algo reached its 90% performance
+            on the respective dataset.
+            -algo_90_performances: the algorithms observed performance, when
+            it first surpassed the 90% threshold.
+            -algo_final_performances: the final performance of algo on dataset
+
+        """
         # read out the time of convergence 90%
-        algo_convergences = dict()
+        algo_convergences90_time = dict()
         algo_final_performances = dict()
         algo_90_performance = dict()
-        for algo_id, curve_dict in self.algo_valid_learning_curves.items():
-            algo_convergences[algo_id] = pd.Series(
+        for algo_id, curve_dict in algo_curves.items():
+            algo_convergences90_time[algo_id] = pd.Series(
                 {k: curve.convergence90_time for k, curve in curve_dict.items()})
             algo_final_performances[algo_id] = pd.Series(
                 {k: curve.final_performance for k, curve in curve_dict.items()})
@@ -139,9 +140,9 @@ class Dataset_Gravity(Dataset):
                 {k: curve.convergence90_performance for k, curve in curve_dict.items()})
 
         # time at which the algo converged on the respective dataset
-        self.algo_convergences90 = pd.DataFrame(algo_convergences)
-        self.algo_convergences90.columns.name = 'Algorithms'
-        self.algo_convergences90.index.name = 'Dataset'
+        self.algo_convergences90_time = pd.DataFrame(algo_convergences90_time)
+        self.algo_convergences90_time.columns.name = 'Algorithms'
+        self.algo_convergences90_time.index.name = 'Dataset'
 
         # how well did the algo perform on the respective dataset
         self.algo_final_performances = pd.DataFrame(algo_final_performances)
@@ -153,24 +154,49 @@ class Dataset_Gravity(Dataset):
         self.algo_90_performances.columns.name = 'Algorithms'
         self.algo_90_performances.index.name = 'Dataset'
 
-        # compute dataset properties  ------------------------------------------
+    def _preprocess_meta_features(self, dataset_meta_features):
+        """create a df with meta features of each dataset"""
+        # Preprocess dataset meta data (remove the indiscriminative string variables)
+        datasets_meta_features_df = pd.DataFrame(
+            list(dataset_meta_features.values()), index=dataset_meta_features.keys()
+        )
+        string_typed_variables = [
+            "usage", "name", "task", "target_type", "feat_type", "metric",
+        ]
+        other_columns = list(set(datasets_meta_features_df.columns) - set(string_typed_variables))
+        datasets_meta_features_df = datasets_meta_features_df[other_columns].astype(float)
+
+        # min-max normalization of numeric features
+        df = datasets_meta_features_df
+        datasets_meta_features_df = (df - df.min()) / (df.max() - df.min())
+        self.datasets_meta_features_df = datasets_meta_features_df
+        self.datasets_meta_features = torch.tensor(
+            self.datasets_meta_features_df.values, dtype=torch.float32)
+
+    def _preprocess_dataset_properties(self, learning_curves, dataset_meta_features):
+        """
+        Computes an object for each dataset and aggregates information for it.
+        :param learning_curves:
+        :return:
+        """
+
         class Datum:
             # placeholder object to keep track of aggregation properties (instead of
             # all of the dictionaries flying around.
             pass
 
         self.dataset_learning_properties = {
-            d: Datum() for d in dataset_meta_features.keys()
+            d: Datum() for d in learning_curves.keys()
         }
-        k = 3
-        for d in validation_learning_curves.keys():
+        b = 3  # number of points to konsider in the topk_available trials
+        for d in learning_curves.keys():
             datum = self.dataset_learning_properties[d]
             datum.final_scores = []  # unconditional distribution
             datum.convergence_step = []
             datum.ranking = []
             datum.meta = dataset_meta_features[d]
 
-            for a, curve in validation_learning_curves[d].items():
+            for a, curve in learning_curves[d].items():
                 datum.convergence_step.append(curve.convergence90_step)
                 # datum.convergence_avg_topk =
                 datum.final_scores.append(curve.final_performance)
@@ -186,31 +212,33 @@ class Dataset_Gravity(Dataset):
 
             datum.ranking = sorted(datum.ranking, key=lambda x: -x[1])
 
-            datum.convergence_avg_topk = np.mean(
-                [algotup[3] for algotup in datum.ranking[:k]]
+            datum.convergence_avg_topb = np.mean(
+                [algotup[3] for algotup in datum.ranking[:b]]
             )
             datum.convergence_avg = np.mean(datum.convergence_step)
 
             datum.topk_available_trials = (
-                    float(datum.meta["time_budget"]) / datum.convergence_avg_topk
+                    float(datum.meta["time_budget"]) / datum.convergence_avg_topb
             )
 
-        # # amount of time on the datasets to find out which algo works best:
-        # plt.hist(self.df_data_meta_features['time_budget'])
-        # plt.show()
-        #
-        # plt.hist([datum.topk_available_trials for datum in self.dataset_learning_properties.values()])
-        # plt.show()
-        order = self.df_data_meta_features.index
+    def _preprocess_thresholded_algo_performances(self, k):
+        """
 
-        algo_performances = pd.DataFrame(
-            [self.dataset_learning_properties[i].final_scores for i in order],
-            index=order,
-        )  # index = dataset_id, column = algo_id
+        :param k: topk (how many algorithm performances are used for comparison in cosine similarity)
+        all others that are not in topk performing on a dataset will be set to 0
+        :return:
+        """
+        # order = self.datasets_meta_features_df.index
+        # algo_performances = pd.DataFrame(
+        #     [self.dataset_learning_properties[i].final_scores for i in order],
+        #     index=order,
+        # )  # index = dataset_id, column = algo_id
+        #
+        # self.algo_final_performances.columns
 
         # optional thresholding: remove k least performing
-        algo_performances = torch.tensor(algo_performances.values, dtype=torch.float32)
-        _, ind = torch.topk(algo_performances, k=10, largest=False, dim=1)
+        algo_performances = torch.tensor(self.algo_final_performances.values, dtype=torch.float32)
+        _, ind = torch.topk(algo_performances, k=k, largest=False, dim=1)
         for i_row, row in zip(ind, algo_performances):
             row[i_row] = 0
 
