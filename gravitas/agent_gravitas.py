2f6b76b5cb1ec56851a2a6ad7a6cea3e1e4ffa5a
timruhkopf <timruhkopf@gmail.com>
2022-02-15 12:02:14 +0100

    moved check_diversity to utils

--- gravitas/agent_gravitas.py
+++ gravitas/agent_gravitas.py
@@ -3,28 +3,12 @@ import torch
 from torch.utils.data import DataLoader
 
 import matplotlib.pyplot as plt
-import numpy as np
+
 
 from gravitas.autoencoder import AE
-from gravitas.base_encoder import BaseEncoder
 from gravitas.vae import VAE
 from gravitas.dataset_gravitas import Dataset_Gravity
-
-
-def check_diversity(representation, title, epsilon=0.01):
-    """
-
-    :param representation: ndarray.
-    :param title: name of the matrix
-    :param epsilon: float: the value needed to exceed (should be close to zero)
-    :raises: Warning if representation is not diverse
-    """
-    # Check for (naive) representation collapse by checking sparsity after
-    # translation by 90% quantile
-    translated = representation - np.quantile(representation, 0.9, axis=0)
-    sparsity = (translated < epsilon).sum() / np.product(representation.shape)
-    if sparsity >= 0.95:
-        raise Warning(f'The {title} representation is not diverse.')
+from gravitas.utils import check_diversity
 
 
 class Agent_Gravitas:
@@ -353,7 +337,6 @@ class Agent_Gravitas:
         plt.savefig(
             f'{self.root_dir}/output/{self.encoder}_training_loss.png',
         )
-
 
     def plot_current_embedding(self, normalize=False):
         """
--- gravitas/utils.py
+++ gravitas/utils.py
@@ -1,13 +1,16 @@
-import torch
+import numpy as np
 
-def cosine_similarity(A, B):
+def check_diversity(representation, title, epsilon=0.01):
     """
-    Cosine_similarity between two matrices
-    :param A: 2d tensor
-    :param B: 2d tensor
-    :return:
+
+    :param representation: ndarray.
+    :param title: name of the matrix
+    :param epsilon: float: the value needed to exceed (should be close to zero)
+    :raises: Warning if representation is not diverse
     """
-    return torch.mm(
-        torch.nn.functional.normalize(A),
-        torch.nn.functional.normalize(B).T
-    )
+    # Check for (naive) representation collapse by checking sparsity after
+    # translation by 90% quantile
+    translated = representation - np.quantile(representation, 0.9, axis=0)
+    sparsity = (translated < epsilon).sum() / np.product(representation.shape)
+    if sparsity >= 0.95:
+        raise Warning(f'The {title} representation is not diverse.')
