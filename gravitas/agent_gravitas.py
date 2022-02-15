4583eb071bd5de815732fba3173efb3cf34ea0f7
timruhkopf <timruhkopf@gmail.com>
2022-02-15 09:45:15 +0100

    added an epsilon to check_sparsity

--- gravitas/agent_gravitas.py
+++ gravitas/agent_gravitas.py
@@ -11,17 +11,18 @@ from gravitas.vae import VAE
 from gravitas.dataset_gravitas import Dataset_Gravity
 
 
-def check_sparsity(representation, title):
+def check_sparsity(representation, title, epsilon=0.01):
     """
 
     :param representation: ndarray.
     :param title: name of the matrix
+    :param epsilon: float: the value needed to exceed (should be close to zero)
     :raises: Warning if representation is not diverse
     """
     # Check for (naive) representation collapse by checking sparsity after
     # translation by 90% quantile
     translated = representation - np.quantile(representation, 0.9, axis=0)
-    sparsity = (translated == 0).sum() / np.product(representation.shape)
+    sparsity = (translated < epsilon).sum() / np.product(representation.shape)
     if sparsity >= 0.95:
         raise Warning(f'The {title} representation is not diverse.')
 
