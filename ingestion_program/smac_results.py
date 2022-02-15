f3297d5b4e30de58de18132eddbb29814d9717ac
timruhkopf <timruhkopf@gmail.com>
2022-02-15 11:48:00 +0100

    refactored smac_results a little & added way to rerun successful configs based on their dataframe selection

--- ingestion_program/smac_results.py
+++ ingestion_program/smac_results.py
@@ -4,7 +4,6 @@ import pandas as pd
 from itertools import chain
 from ingestion_program.ingestion_smac import tae, kf, env  # just for explicitness
 
-
 run_id = '1608637542_vae'
 
 root_dir = '/'.join(os.getcwd().split('/')[:-1])  # fixing the root to project root and not ingestion_program
@@ -30,14 +29,20 @@ config_df.index = config_df.index.astype(int)
 run_config_df = pd.merge(config_df, run_df, left_index=True, right_on='config_id')
 
 # Find the not failed configs on highest fidelity.
-final_fidelity = run_config_df[(run_df['budget'] == 10000) & (run_df['status'] == 'StatusType.SUCCESS')]
+budget_success = (run_df['budget'] == 10000) & (run_df['status'] == 'StatusType.SUCCESS')
+final_fidelity = run_config_df[budget_success]
 final_fidelity = final_fidelity[['cost', 'budget', 'status', *config_df.columns]].sort_values('cost')
 
 # Write out frame to csv.
 # run_config_df.to_csv(f'{root_dir}/output/run_{run_id}/run_{run_id}.csv')
+config_names = ['embedding_dim', 'learning_rate_init', 'lossweight1',
+                'lossweight2', 'lossweight3', 'lossweight4', 'n_compettitors',
+                'repellent_share']
 
+# index is runid!
+rerun_config = final_fidelity[config_names].transpose().to_dict()
 
-cfg = configs['1']  # define the
-
+# single config rerun of TAE:
 # TODO create an ID, with which the models are plotted and saved.
+cfg = configs['1']
 tae(cfg, budget=100)
